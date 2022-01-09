e5dd13e0535ce874760393924976cc4ff495f28f
glebvi1 <vyazgd@mail.ru>
2022-01-09 20:45:41 +0300

    update qblock

---
+++ res/maps/Money.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Money" tilewidth="20" tileheight="20" tilecount="1" columns="1">
+ <image source="../blocks/money.png" width="20" height="20"/>
+</tileset>
