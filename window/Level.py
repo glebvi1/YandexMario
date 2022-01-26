6dcefda3a671c1e469e8cfff5293a13d93d2aad9
glebvi1 <vyazgd@mail.ru>
2022-01-26 15:52:23 +0300

    optimize

--- window/Level.py
+++ window/Level.py
@@ -67,7 +67,6 @@ class Level:
             self.mario = Mario(coords, MARIO_PATH)
         elif mo_id == 5:
             self.blocks.append(QBlock(coords, QBLOCKS_PATH))
-
         elif mo_id == 6:
             self.enemies.append(FlyDeath(coords, FLY_DEATH_PATH))
         elif mo_id == 7:
